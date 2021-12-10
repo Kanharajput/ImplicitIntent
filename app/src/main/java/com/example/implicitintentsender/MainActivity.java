ccd2b9c2fee6094c3eed04dbb5134f9610fd11e4
Kanha Tomar <kanharajput91@gmail.com>
2021-12-10 05:36:58 -0500

    Receiving Intent

--- app/src/main/java/com/example/implicitintentsender/MainActivity.java
+++ app/src/main/java/com/example/implicitintentsender/MainActivity.java
@@ -9,61 +9,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    // object to get the link from the user
-    private EditText msiteEditText;
-    private EditText mLocationEditText;
-    private EditText mMessageEditText;
+    private TextView mUrlTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        msiteEditText = findViewById(R.id.url);
-        mLocationEditText = findViewById(R.id.loc);
-        mMessageEditText = findViewById(R.id.mess);
-    }
+        Intent intent = getIntent();
 
-    // onclick methods
-    public void openSite(View view) {
-        String url_by_user;     // to store the data which is passed to the screen by the user
-        // get the data
-        url_by_user = msiteEditText.getText().toString();
-        Uri webpage = Uri.parse(url_by_user);
-        Intent intent = new Intent(Intent.ACTION_VIEW,webpage);       // action view to view data happing with our data that is webpage
-        // check whether there(android device) has an acitivity to handle our intent(action and data both) or not
-        if(intent.resolveActivity(getPackageManager()) != null) {      // package manager find activity satisifying our intent
-            startActivity(intent);
+        Uri uri = intent.getData();
+        if(uri != null) {
+            String uri_String = getString(R.string.urlLabel) + uri.toString();
+            mUrlTextView = findViewById(R.id.txt_here);
+            mUrlTextView.setText(uri_String);
         }
-        else {
-            Log.d("ImplicitIntent", "No-one to handle intent");
-        }
-    }
-
-    public void openLocation(View view) {
-        String location;
-        location = mLocationEditText.getText().toString();
-        Uri adressUri = Uri.parse("geo:0,0?q=" + location);      // query string for general location other option is to use lattitude & longitude
-        Intent intent = new Intent(Intent.ACTION_VIEW,adressUri);
-
-        if(intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
-        else {
-            Log.d("ImplicitIntent","No-one to handle");
-        }
-    }
-
-    public void shareMessage(View view) {
-        String message;
-        message = mMessageEditText.getText().toString();
-        String mimeType = "text/plain";
-        ShareCompat.IntentBuilder
-                .from(this)
-                .setType(mimeType)
-                .setChooserTitle(R.string.chooserMessage)
-                .setText(message)
-                .startChooser();
     }
 }
